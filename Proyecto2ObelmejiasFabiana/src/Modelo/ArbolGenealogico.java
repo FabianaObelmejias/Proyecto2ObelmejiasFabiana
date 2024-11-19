60db880a9f07bfdfaa831033bd8ab1daa48a96b2
FabianaObelmejias <fabiana.obelmejias@correo.unimet.edu.ve>
2024-11-18 23:10:32 -0400

    Base de ArbolGenealogico
    Según las instrucciones del proyecto, dejo las posibles funcionalidades más importantes de esta clase, más adelante (según sea necesario) se extenderán, reducirán o acomodarán.

--- Proyecto2ObelmejiasFabiana/src/Modelo/ArbolGenealogico.java
+++ Proyecto2ObelmejiasFabiana/src/Modelo/ArbolGenealogico.java
@@ -4,10 +4,45 @@
  */
 package Modelo;
 
+import EDD.ArbolGeneral;
+import EDD.HashTable;
+import EDD.Lista;
+
 /**
  *
  * @author obelm
  */
 public class ArbolGenealogico {
+    private ArbolGeneral arbol;
+    private HashTable hashtable;
+
+    public ArbolGenealogico(ArbolGeneral arbol, HashTable hashtable) {
+        this.arbol = arbol;
+        this.hashtable = hashtable;
+    }
     
+    //es necesario agregar una persona
+    public void agregarPersona(Persona persona){
+        
+    }
+    
+    public void mostrarAntepasados(Persona persona){
+        
+    }
+    
+    public void buscarNombre(String nombre){
+        
+    }
+    
+    public void buscarTitulo(String titulo){
+        
+    }
+    
+    public void mostrarGeneracion(int generacion){
+        
+    }
+    
+    public Lista opcionesDeGeneracion(){
+        return null;
+    }
 }
