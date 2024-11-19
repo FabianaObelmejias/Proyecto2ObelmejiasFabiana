7531a6a72f9f9c7985d9f4a107be53b319dffe1e
FabianaObelmejias <fabiana.obelmejias@correo.unimet.edu.ve>
2024-11-18 21:28:18 -0400

    Arreglo en clase NodoArbol
    Me equivoqué en la clase a programar, me daba un constante error y no entendía por qué, me di cuenta que esta trabajando sobre la clase que no era

--- Proyecto2ObelmejiasFabiana/src/EDD/NodoArbol.java
+++ Proyecto2ObelmejiasFabiana/src/EDD/NodoArbol.java
@@ -9,5 +9,48 @@ package EDD;
  * @author obelm
  */
 public class NodoArbol {
+    //primero se crea el nodo del árbol
+    private Object dato;
+    private NodoArbol padre; // no esta en las instrucciones pero es importante para que cada nodo sepa quie es su padre
+    private Lista hijos;
+
+    public NodoArbol(Object dato) { //solo necesito el dato
+        this.dato = dato;
+        this.padre = null; //el padre cuando lo cree en un principio va a ser nulo
+        this.hijos = new Lista();
+    }
+
+    public Object getDato() {
+        return dato;
+    }
+
+    public void setDato(Object dato) {
+        this.dato = dato;
+    }
+
+    public NodoArbol getPadre() {
+        return padre;
+    }
+
+    public void setPadre(NodoArbol padre) {
+        this.padre = padre;
+    }
+
+    public Lista getHijos() {
+        return hijos;
+    }
+
+    public void setHijos(Lista hijos) {
+        this.hijos = hijos;
+    }
     
+    public void agregarHijo(NodoArbol hijo){//funcion para agregar hijo
+        hijo.setPadre(this);
+        this.hijos.insertarFinal(hijo);
+    }
+            
+            
+    public boolean esHoja(){//funcion para verificar si tiene hijos o no 
+        return hijos.isEmpty();
+}
 }
