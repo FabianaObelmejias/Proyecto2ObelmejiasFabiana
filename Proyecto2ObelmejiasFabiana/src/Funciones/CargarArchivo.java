e5be06375ebeb9ce9e325c6166eda9ab287267a7
FabianaObelmejias <fabiana.obelmejias@correo.unimet.edu.ve>
2024-11-19 20:17:36 -0400

    funcion cargar el Json
    con todas las bibliotecas importadas

--- Proyecto2ObelmejiasFabiana/src/Funciones/CargarArchivo.java
+++ Proyecto2ObelmejiasFabiana/src/Funciones/CargarArchivo.java
@@ -5,6 +5,12 @@
 package Funciones;
 
 import Modelo.ArbolGenealogico;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import java.io.FileReader;
+import java.io.IOException;
 
 /**
  *
@@ -24,6 +30,27 @@ public class CargarArchivo {
     public void setArbolGenealogico(ArbolGenealogico arbolGenealogico) {
         this.arbolGenealogico = arbolGenealogico;
     }
+
+    public void cargar(String rutaArchivo) {
+        try (FileReader reader = new FileReader(rutaArchivo)) {
+            Gson gson = new Gson();
+            JsonObject jsonObj = gson.fromJson(reader, JsonObject.class);//con esto puedo iterar sobre el Json
+            
+            for (String nombreCasa : jsonObj.keySet()) {
+                JsonArray miembros = jsonObj.getAsJsonArray(nombreCasa); //ya que tengo todos los values, puedo iterar sobre cada uno de los miembros
+                for (JsonElement miembro : miembros) {
+                    JsonObject personaObj = miembro.getAsJsonObject();
+                    
+                }
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error al leer el archivo: " + e.getMessage());
+        }
+    }
+
+
+
 
 
 }
